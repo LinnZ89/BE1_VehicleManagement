474c782ebd9f4fc8eac819148125767315d3c306
Qorilz89._ <90020801+LinnZ89@users.noreply.github.com>
2024-06-04 21:41:13 +0700

    REFIX DISPLAY MENU + DISPLAYALL FUNCTION
    SAVETOFILE AND READFILE STILL NON FIXED

--- controllers/VehicleList.java
+++ controllers/VehicleList.java
@@ -33,14 +33,14 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
             if (sc.hasNextDouble()) {
                 price = sc.nextDouble();
                 if (price > 0) {
-                    sc.nextLine(); 
+                    sc.nextLine();
                     break;
                 } else {
                     System.out.println("Price must be greater than 0. Try again.");
                 }
             } else {
                 System.out.println("Invalid input. Enter a valid price.");
-                sc.next(); 
+                sc.next();
             }
         }
 
@@ -51,67 +51,79 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
         String type = sc.nextLine();
 
         System.out.println("Enter vehicle's productYear: ");
-        Integer productYear = sc.nextInt();
+        int productYear = sc.nextInt();
+        sc.nextLine(); // Consume the remaining newline
 
         Vehicle vehicle = new Vehicle(code, name, color, price, brand, type, productYear);
-
         this.add(vehicle);
 
-        System.out.println("Vehicle was added succesfully!");
+        System.out.println("Vehicle was added successfully!");
     }
 
     @Override
     public void remove() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter vehicle's code to update: ");
-        String searchingcode = sc.nextLine();
+        System.out.println("Enter vehicle's code to remove: ");
+        String searchingCode = sc.nextLine();
 
-        VehicleList vehicleList = this;
-        for (Vehicle vehicle : vehicleList) {
-            if (vehicle.getId() == searchingcode) {
-
+        Vehicle toRemove = null;
+        for (Vehicle vehicle : this) {
+            if (vehicle.getId().equals(searchingCode)) {
+                toRemove = vehicle;
+                break;
             }
         }
 
+        if (toRemove != null) {
+            this.remove(toRemove);
+            System.out.println("Vehicle removed successfully!");
+        } else {
+            System.out.println("Vehicle not found!");
+        }
     }
-    
+
     @Override
     public void update() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter vehicle's code to update: ");
-        String searchingcode = sc.nextLine();
+        String searchingCode = sc.nextLine();
 
-        VehicleList vehicleList = this;
-        for (Vehicle Ivehicle : vehicleList) {
-            if (Ivehicle.getId() == searchingcode) {
+        Vehicle toUpdate = null;
+        for (Vehicle vehicle : this) {
+            if (vehicle.getId().equals(searchingCode)) {
+                toUpdate = vehicle;
+                break;
+            }
+        }
 
-                System.out.println("Enter new vehicle's code: ");
-                String code = sc.nextLine();
+        if (toUpdate != null) {
+            System.out.println("Enter new vehicle's code: ");
+            String code = sc.nextLine();
 
-                System.out.println("Enter new vehicle's name: ");
-                String name = sc.nextLine();
+            System.out.println("Enter new vehicle's name: ");
+            String name = sc.nextLine();
 
-                System.out.println("Enter new vehicle's color: ");
-                String color = sc.nextLine();
+            System.out.println("Enter new vehicle's color: ");
+            String color = sc.nextLine();
 
-                double price = 0;
-                while (true) {
+            double price = 0;
+            while (true) {
                 System.out.println("Enter new vehicle's price: ");
                 if (sc.hasNextDouble()) {
                     price = sc.nextDouble();
                     if (price > 0) {
-                        sc.nextLine(); 
+                        sc.nextLine();
                         break;
                     } else {
                         System.out.println("Price must be greater than 0. Try again.");
                     }
                 } else {
                     System.out.println("Invalid input. Enter a valid price.");
-                    sc.next(); 
-                    }
+                    sc.next();
                 }
+            }
 
             System.out.println("Enter new vehicle's brand: ");
             String brand = sc.nextLine();
@@ -120,66 +132,70 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
             String type = sc.nextLine();
 
             System.out.println("Enter new vehicle's productYear: ");
-            Integer productYear = sc.nextInt();
+            int productYear = sc.nextInt();
+            sc.nextLine(); // Consume the remaining newline
 
-            Vehicle vehicle = new Vehicle(code, name, color, price, brand, type, productYear);
-                }
-            }
+            toUpdate.setId(code);
+            toUpdate.setName(name);
+            toUpdate.setColor(color);
+            toUpdate.setPrice(price);
+            toUpdate.setBrand(brand);
+            toUpdate.setType(type);
+            toUpdate.setProductYear(productYear);
+
+            System.out.println("Vehicle updated successfully!");
+        } else {
+            System.out.println("Vehicle not found!");
         }
+    }
 
     @Override
     public void savetoFile() {
-
         try (FileOutputStream fileOutput = new FileOutputStream("data\\Vehicle.txt");
-            ObjectOutputStream write = new ObjectOutputStream(fileOutput)) {
-                write.writeObject(this);
-                System.out.println("Saved Successfully!");
-            } catch (IOException exception) {
-                System.out.println("Saved Failed!");
-            }
-
+             ObjectOutputStream write = new ObjectOutputStream(fileOutput)) {
+            write.writeObject(this);
+            System.out.println("Saved Successfully!");
+        } catch (IOException exception) {
+            System.out.println("Save Failed!");
+        }
     }
 
     @Override
     public VehicleList readFile() {
-
         VehicleList res = new VehicleList();
         try (FileInputStream fileInput = new FileInputStream("data\\Vehicle.txt");
-            ObjectInputStream read = new ObjectInputStream(fileInput)) {
-
-                res = (VehicleList) read.readObject();
-                System.out.println("Read Successfully!");
-
-            } catch (EOFException eofException) {
-                System.out.println("Read Failed!");
-
-            } catch (IOException | ClassNotFoundException exception) {
-
-            }
-            return res;
+             ObjectInputStream read = new ObjectInputStream(fileInput)) {
+            res = (VehicleList) read.readObject();
+            System.out.println("Read Successfully!");
+        } catch (EOFException eofException) {
+            System.out.println("Read Failed!");
+        } catch (IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+        return res;
     }
 
     @Override
     public void search_by_code() {
         Scanner sc = new Scanner(System.in);
-        System.err.println("Enter vehicle's code: ");
-        String searchingcode = sc.nextLine();
-        VehicleList vehicleList = this;
-        for (Vehicle vehicle : vehicleList) {
-            if (vehicle.getId() == searchingcode) {
-                vehicle.toString();
+        System.out.println("Enter vehicle's code: ");
+        String searchingCode = sc.nextLine();
+        for (Vehicle vehicle : this) {
+            if (vehicle.getId().equals(searchingCode)) {
+                System.out.println(vehicle.toString());
+                return;
             }
         }
+        System.out.println("Vehicle not found!");
     }
 
     @Override
     public boolean check() {
         Scanner sc = new Scanner(System.in);
-        System.err.println("Enter vehicle's code: ");
-        String searchingcode = sc.nextLine();
-        VehicleList vehicleList = this;
-        for (Vehicle vehicle : vehicleList) {
-            if (vehicle.getId() == searchingcode) {
+        System.out.println("Enter vehicle's code: ");
+        String searchingCode = sc.nextLine();
+        for (Vehicle vehicle : this) {
+            if (vehicle.getId().equals(searchingCode)) {
                 return true;
             }
         }
@@ -188,22 +204,24 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
 
     @Override
     public void search_by_name() {
-        VehicleList vehicleList = this;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter vehicle's name: ");
-        String searchingname = sc.nextLine();
-        Integer count = 0;
-        for (Vehicle vehicle : vehicleList) {
-            if (vehicle.getName() == searchingname) {
+        String searchingName = sc.nextLine();
+        int count = 0;
+        for (Vehicle vehicle : this) {
+            if (vehicle.getName().equals(searchingName)) {
                 System.out.println("___VEHICLE " + count + "___");
-                vehicle.toString();
+                System.out.println(vehicle.toString());
+                count++;
             }
+        }
+        if (count == 0) {
+            System.out.println("No vehicle found with the given name!");
         }
     }
 
     @Override
     public void searchmenu() {
-        VehicleList vehicleList = this;
         while (true) {
             System.out.println("______Searching Menu______");
             System.out.println("1. SEARCH BY NAME.");
@@ -211,18 +229,18 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
             System.out.println("3. GO BACK TO MAIN MENU.");
 
             Scanner sc = new Scanner(System.in);
-            Integer choice = sc.nextInt();
+            int choice = sc.nextInt();
             sc.nextLine();
 
-            switch(choice) {
+            switch (choice) {
                 case 1:
-                    vehicleList.search_by_name();
+                    search_by_name();
                     break;
                 case 2:
-                    vehicleList.search_by_code();
+                    search_by_code();
                     break;
                 case 3:
-                    break;
+                    return;
                 default:
                     System.out.println("Invalid choice. Please choose again!");
             }
@@ -231,7 +249,6 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
 
     @Override
     public void displaymenu() {
-        VehicleList vehicleList = this;
         while (true) {
             System.out.println("______Display Menu______");
             System.out.println("1. DISPLAY ALL.");
@@ -239,18 +256,18 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
             System.out.println("3. GO BACK TO MAIN MENU.");
 
             Scanner sc = new Scanner(System.in);
-            Integer choice = sc.nextInt();
+            int choice = sc.nextInt();
             sc.nextLine();
 
-            switch(choice) {
+            switch (choice) {
                 case 1:
-                    vehicleList.displayall();
+                    displayall();
                     break;
                 case 2:
-                    vehicleList.displaybytype();
+                    displaybytype();
                     break;
                 case 3:
-                    break;
+                    return;
                 default:
                     System.out.println("Invalid choice. Please choose again!");
             }
@@ -259,112 +276,79 @@ public class VehicleList extends ArrayList<Vehicle> implements VehicleManagement
 
     @Override
     public void displayall() {
-        VehicleList vehicleList = this;
-        int count = 0;
-        if (vehicleList.isEmpty() == true) {
+        if (this.isEmpty()) {
             System.out.println("THERE IS NO VEHICLE IN SHOWROOM!");
             return;
-        } else {
-            int n = vehicleList.size();
-            Vehicle[] temp = new Vehicle[n];
-      
-            for (int i = 0; i < n; i++) {
-                temp[i] = vehicleList.get(i);
-            }
-      
-            for (int i = 0; i < n - 1; i++) {
-                int maxIndex = i;
-                for (int j = i + 1; j < n; j++) {
-                    if (temp[j].getPrice() > temp[maxIndex].getPrice()) {
-                        maxIndex = j;
-                    }
-                }
-            
-                    Vehicle tempVar = temp[i];
-                    temp[i] = temp[maxIndex];
-                    temp[maxIndex] = tempVar;
-                }
-      
-            for (int i = 0; i < n; i++) {
-                vehicleList.set(i, temp[i]);
-            }
+        }
 
-            for (Vehicle vehicle : vehicleList) {
-                System.out.println("______VEHICLE #______" + count);
-                vehicle.toString();
-                count += 1;
+        Vehicle[] temp = this.toArray(new Vehicle[0]);
+
+        for (int i = 0; i < temp.length - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < temp.length; j++) {
+                if (temp[j].getPrice() > temp[maxIndex].getPrice()) {
+                    maxIndex = j;
+                }
             }
+            Vehicle tempVar = temp[i];
+            temp[i] = temp[maxIndex];
+            temp[maxIndex] = tempVar;
+        }
+
+        int count = 0;
+        for (Vehicle vehicle : temp) {
+            System.out.println("______VEHICLE #______" + count);
+            System.out.println(vehicle.toString());
+            count++;
         }
     }
 
     @Override
     public void displaybytype() {
-        VehicleList vehicleList = this;
-      
-        if (vehicleList.isEmpty()) {
-          System.out.println("THERE IS NO VEHICLE IN SHOWROOM!");
-          return;
-        } else {
-            Scanner sc = new Scanner(System.in);
-            Integer count = 0;
+        if (this.isEmpty()) {
+            System.out.println("THERE IS NO VEHICLE IN SHOWROOM!");
+            return;
+        }
 
-            System.out.println("ENTER VEHICLE'S TYPE: ");
-            String displaytype = sc.nextLine();
-      
-            System.out.println("______KIND OF DISPLAY______");
-            System.out.println("1. NORMAL DISPLAY.");
-            System.out.println("2. DESCENDING DISPLAY.");
-            System.out.println("Please choose!!");
-            Integer choice = sc.nextInt();
-      
-          switch (choice) {
-            case 1:
-              for (Vehicle vehicle : vehicleList) {
-                if (vehicle.getType().equals(displaytype)) {
-                  System.out.println("______VEHICLE #______" + count + ": " + vehicle.toString());
-                  count++;
-                }
-              }
-              break;
-      
-            case 2:
-                int n = vehicleList.size();
-                Vehicle[] temp = new Vehicle[n];
-      
-                for (int i = 0; i < n; i++) {
-                    temp[i] = vehicleList.get(i);
-                }
-      
-                for (int i = 0; i < n - 1; i++) {
-                    int maxIndex = i;
-                    for (int j = i + 1; j < n; j++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER VEHICLE'S TYPE: ");
+        String displayType = sc.nextLine();
+
+        System.out.println("______KIND OF DISPLAY______");
+        System.out.println("1. NORMAL DISPLAY.");
+        System.out.println("2. DESCENDING DISPLAY.");
+        System.out.println("Please choose!!");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        Vehicle[] temp = this.toArray(new Vehicle[0]);
+
+        if (choice == 2) {
+            for (int i = 0; i < temp.length - 1; i++) {
+                int maxIndex = i;
+                for (int j = i + 1; j < temp.length; j++) {
                     if (temp[j].getPrice() > temp[maxIndex].getPrice()) {
                         maxIndex = j;
                     }
-                    }
-            
-                    Vehicle tempVar = temp[i];
-                    temp[i] = temp[maxIndex];
-                    temp[maxIndex] = tempVar;
                 }
-      
-                for (int i = 0; i < n; i++) {
-                    vehicleList.set(i, temp[i]);
-                }
-      
-                for (Vehicle vehicle : vehicleList) {
-                    if (vehicle.getType().equals(displaytype)) {
-                    System.out.println("______VEHICLE #______" + count + ": " + vehicle.toString());
-                    count++;
-                    }
-                }
-                break;
-      
-            default:
-                break;
-            }
+                Vehicle tempVar = temp[i];
+                temp[i] = temp[maxIndex];
+                temp[maxIndex] = tempVar;
             }
         }
+
+        int count = 0;
+        for (Vehicle vehicle : temp) {
+            if (vehicle.getType().equals(displayType)) {
+                System.out.println("______VEHICLE #______" + count + ": " + vehicle.toString());
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("No vehicles found of the specified type!");
+        }
+    }
 
     @Override
     public void displayinfile() {
